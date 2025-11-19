0b1bb2ff3d5b8c2aee1785bd22525cbb3cc2927d
kade-q <102777078+kade-q@users.noreply.github.com>
2025-11-19 14:13:30 -0600

    Update Teil2.vhd

--- Teil2.vhd
+++ Teil2.vhd
@@ -1,19 +1,21 @@
-library ieee;
-use ieee.std_locig_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
-entity teil2 is
-  port (
-    re1, im1, re2, : in signed (7 downto 0) := 0;
+entity top is
+Port (
+    re1, im1, re2, im2 : in signed (7 downto 0) := "00000000";
     re_o, im_o : out signed (7 downto 0)
-    );
-end teil2;
+);
+end top;
 
-architecture behav of teil2 is
+architecture Behavioral of top is
+
 begin
   multiply : process (re1, im1, re2, im2)
     begin
       re_o <= re1 * re2 - im1 * im2;
       im_o <= re1 * im2 + re2 * im1;
     end process multiply;
-end behav;
-      
+
+end Behavioral;
