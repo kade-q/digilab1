acbca53ff6daa613365e005edf44d41e3b36429b
kade-q <102777078+kade-q@users.noreply.github.com>
2025-11-29 11:25:30 -0600

    Update blu.vhd

--- blu.vhd
+++ blu.vhd
@@ -2,8 +2,8 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity blu is
-  Port (operation: in std_logic_vector (1 downto 0);
-    a,b: in std_logic_vector (7 downto 0);
+  Port (operation: in std_logic_vector (1 downto 0):= "00";
+    a,b: in std_logic_vector (7 downto 0) := "00";
     result: out std_logic_vector (7 downto 0)
     );
 end blu;
