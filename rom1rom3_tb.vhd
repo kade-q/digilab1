b3d23e18e971accf7091908a9bfbdc82413d60b1
kade-q <102777078+kade-q@users.noreply.github.com>
2025-12-10 10:47:11 -0600

    Add files via upload

---
+++ rom1rom3_tb.vhd
@@ -0,0 +1,59 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity rom1rom3_tb is
+
+end rom1rom3_tb;
+
+architecture sim of rom1rom3_tb is
+
+component rom1
+    Port (address: in std_logic_vector (2 downto 0);
+        clk: in std_logic;
+        data: out std_logic_vector (7 downto 0) 
+        );
+end component;
+
+component rom3
+  Port (address: in std_logic_vector (2 downto 0);
+    clk: in std_logic;
+    data: out std_logic_vector (7 downto 0) 
+    );
+end component;
+
+signal tb_address: std_logic_vector (2 downto 0):= "000";
+signal tb_clk: std_logic;
+signal tb_data: std_logic_vector (7 downto 0);
+signal next_address: unsigned (2 downto 0):= "000";
+
+begin
+
+    DUT1: rom1 port map (
+        address => tb_address,
+        clk => tb_clk,
+        data => tb_data
+        );
+
+    DUT2: rom3 port map (
+        address => tb_address,
+        clk => tb_clk,
+        data => tb_data
+        );
+        
+   clk_gen : process
+   begin
+        tb_clk <= transport '1' after 50 ns, '0' after 100 ns;
+        wait for 100ns; 
+   end process clk_gen;
+   
+   update_address: process (tb_clk)
+   begin
+        if falling_edge(tb_clk) then
+            tb_address <= std_logic_vector( unsigned (next_address));
+            next_address <= next_address + 1;
+        end if;
+   end process update_address;
+
+end sim;
---
+++ rom3.vhd
@@ -0,0 +1,43 @@
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity rom3 is
+  Port (address: in std_logic_vector (2 downto 0);
+    clk: in std_logic;
+    data: out std_logic_vector (7 downto 0) 
+    );
+end rom3;
+
+architecture rtl of rom3 is
+
+    signal ndata: std_logic_vector (7 downto 0);
+    signal q: std_logic_vector (2 downto 0);
+
+begin
+
+    reg: process(clk)
+    begin
+        if clk'event and clk='1' then
+            data <= ndata;
+            q <= address;
+        end if;
+    end process reg;
+
+    rom: process(q)
+    begin
+        case q is
+            when "000" => ndata <= "00000001";
+            when "001" => ndata <= "00000100";
+            when "010" => ndata <= "00001001";
+            when "011" => ndata <= "00010000";
+            when "100" => ndata <= "00011001";
+            when "101" => ndata <= "00100100";
+            when "110" => ndata <= "00110001";
+            when others => ndata <= "00000000";
+        end case;
+    end process rom;
+
+
+end rtl;
